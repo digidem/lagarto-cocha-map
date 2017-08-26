87f166d4854c00711a759f9e2d65edc7e3bebe6f
karissa <krmckelv@gmail.com>
2017-08-25 18:17:47 -0700

    remove unused language.js for alianza-elements

--- test/language.js
+++
@@ -1,9 +0,0 @@
-var languageSelector = require('../src/language')
-
-function onClick (lang) {
-  console.log('lang', lang)
-}
-
-var el = languageSelector(onClick, 'eng')
-
-document.body.appendChild(el)
